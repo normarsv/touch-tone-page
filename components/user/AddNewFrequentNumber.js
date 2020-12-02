1dd3879ad7460d413d6e3d1f955f79b84d934384
zamarripad <dzamarripa@guaostudio.com>
2020-12-02 17:57:50 -0600

    Frequent numbers (list & add new)

---
+++ components/user/AddNewFrequentNumber.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Col, Modal, Row, Space } from "antd";
+import FormGenerator from "../../components-base/FormGenerator";
+
+const AddNewFrequentNumber = ({
+  frequentNumberForm,
+  visibleNewFrequentNumber,
+  setVisibleNewFrequentNumber,
+}) => {
+  function handleCancel() {
+    // setSelectedOrganization({});
+    // setProvideOrganizations([]);
+    setVisibleNewFrequentNumber();
+  }
+
+  return (
+    <Modal
+      visible={visibleNewFrequentNumber}
+      centered
+      onCancel={() => handleCancel()}
+      footer={null}
+    >
+      <Space direction="vertical" size="large">
+        <h2 className="title-style">Add Frequent Number</h2>
+        <FormGenerator FormOptions={frequentNumberForm} />
+      </Space>
+    </Modal>
+  );
+};
+
+AddNewFrequentNumber.propTypes = {
+  someData: PropTypes.string,
+};
+
+export default AddNewFrequentNumber;
