65a134cf577e473d2a93e84a09d74154bc980c28
Javier Santos <jsantos@guaodev.com>
2021-03-04 10:38:49 -0800

    Update logo

--- components/base/HeaderMenu.js
+++ components/base/HeaderMenu.js
@@ -113,7 +113,7 @@ const HeaderMenu = ({ mainBodyRef, openSideMenu }) => {
           }}
           id='menu-header-logo'
         >
-          <img className='page-side-menu-logo' alt="TouchTone Communications" src={'/logo-portal.png'} />
+          <img className='page-side-menu-logo' alt="TouchTone Communications" src={'/Logo-Portal.png'} />
         </a>
       </Col>
       <Col flex='50%'>
